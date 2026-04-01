8f30f328a63298179c911b375058f65f2e52ef26
aimtosucess360-hub <aimtosucess360@gmail.com>
2026-04-01 17:46:21 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'career-counselling-cpbu.vercel.app'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
